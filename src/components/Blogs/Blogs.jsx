0241d5f6a53731c5c9d780fa1e45ad20e3ec1011
sayed_hossain <sayed.hasan0011@gmail.com>
2023-09-15 01:53:39 +0600

    React Blog Website

---
+++ src/components/Blogs/Blogs.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+const Blogs = () => {
+
+    const [blogs, setBlogs] = useState([])
+    useEffect(() => {
+        fetch('blogs.json')
+        .then(res => res.json())
+        .then(data => setBlogs(data))
+    }, [])
+    return (
+        <div>
+            <h3>Blogs: {blogs.length}</h3>
+            {
+                blogs.map(blog => <h3 key={blog.id}> {blog.title} </h3> )
+            }
+        </div>
+    );
+};
+
+export default Blogs;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-6xl'>Knowledge Base</h1>
+        </div>
+    );
+};
+
+export default Header;
